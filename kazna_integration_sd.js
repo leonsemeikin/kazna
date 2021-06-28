c1230443dae6d8df4377ace406861fac51a05771
Семейкин Леонид <lsemejkin@naumen.ru>
2021-06-28 20:36:40 +0500

    Revert "version 3"
    This reverts commit e1c954114f1b540d61c4f75d2b33b19fdbb0f088.

--- kazna_integration_sd.js
+++ kazna_integration_sd.js
@@ -1,25 +1,26 @@
-const sleep = ms => {
+function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const checkEmp = () => {
-    let idemp = JQ('#acf_employee input').val();
-    let idnewemp = JQ('#acf_newEmployee input').val();
+function checkEmp() {
+    let idemp = jQuery('#acf_employee input').val();
+    let idnewemp = jQuery('#acf_newEmployee input').val();
     if (idemp) {
         atremployee = idemp;
-    } else if (idnewemp) { atremployee = idnewemp; }
+        console.log('employeeEXIST')
+    } else if (idnewemp) { atremployee = idnewemp; console.log('newEmployeeEXIST'); }
     else { atremployee = '' }
 }
 
-JQ(document).ready(function () {
+jQuery(document).ready(function () {
     try {
         let link;
         JQ('#acf_cf4').hide();
         JQ('#b1').hide();
-        jQ('#acf_cf3group001f_sw').click();
-        jQ('#acf_cf1group001f_sw').click();
-        jQ('#acf_cf2group001f_sw').click();
-        atremployee = JQ('#acf_employee input').val();
+        jQuery('#acf_cf3group001f_sw').click();
+        jQuery('#acf_cf1group001f_sw').click();
+        jQuery('#acf_cf2group001f_sw').click();
+        atremployee = jQuery('#acf_employee input').val();
 
         //iframe запросы пользователя
         JQ('#acf_cf3group001f_sw').click(function () {
@@ -27,17 +28,19 @@ JQ(document).ready(function () {
             let re = /.+\$/g;
             let emplnumber = atremployee.replace(re, '');
             if (emplnumber) {
-                JQ('#acf_fv1 iframe')[0].src = 'http://sm-sue.fsfk.local/sd/operator/?anchor=#uuid%3Aemployee%24' + emplnumber + '!%7B%22tab%22:%22b661b970-470d-404c-14f6-1144fae3f071,915d5924-e9a1-7262-ec81-3d08a3a87cf0%22%7D';
+                jQuery('#acf_fv1 iframe')[0].src = 'http://sm-sue.fsfk.local/sd/operator/?anchor=#uuid%3Aemployee%24' + emplnumber + '!%7B%22tab%22:%22b661b970-470d-404c-14f6-1144fae3f071,915d5924-e9a1-7262-ec81-3d08a3a87cf0%22%7D';
             }
             else { alert('Клиент по таким данным в SD не найден') }
         });
+        //iframe запросы пользователя
 
         //iframe база знаний
         JQ('#acf_cf1group001f_sw').click(function () {
-            atremployeeOperator = JQ('#acf_employeeOperator input').val();
+            atremployeeOperator = jQuery('#acf_employeeOperator input').val();
             let re = /.+\$/g;
             let empOperlnumber = atremployeeOperator.replace(re, '');
-            JQ('#acf_fv2 iframe')[0].src = 'http://sm-sue.fsfk.local/sd/operator/#uuid:employee$' + empOperlnumber + '!%7B%22tab%22:%22bb3c9c55-1787-2d08-c323-07a01ec644c6%22%7D'
+            jQuery('#acf_fv2 iframe')[0].src = 'http://sm-sue.fsfk.local/sd/operator/#uuid:employee$' + empOperlnumber + '!%7B%22tab%22:%22bb3c9c55-1787-2d08-c323-07a01ec644c6%22%7D'
+
         });
 
         //iframe создать запрос
@@ -46,10 +49,11 @@ JQ(document).ready(function () {
             let re = /.+\$/g;
             let emplnumber = atremployee.replace(re, '');
             if (emplnumber) {
-                JQ('#acf_fv3 iframe')[0].src = 'http://sm-sue.fsfk.local/sd/operator/#add%3AserviceCall%24request%3Aemployee%24' + emplnumber + '%3Arequest!%7B%22fast%22:%22true%22%7D';
+                jQuery('#acf_fv3 iframe')[0].src = 'http://sm-sue.fsfk.local/sd/operator/#add%3AserviceCall%24request%3Aemployee%24' + emplnumber + '%3Arequest!%7B%22fast%22:%22true%22%7D';
             }
-            else { JQ('#acf_fv3 iframe')[0].src = 'http://sm-sue.fsfk.local/sd/operator/#add%3AserviceCall%24request%3A%3Arequest!%7B%22fast%22:%22true%22%7D' }
+            else { jQuery('#acf_fv3 iframe')[0].src = 'http://sm-sue.fsfk.local/sd/operator/#add%3AserviceCall%24request%3A%3Arequest!%7B%22fast%22:%22true%22%7D' }
         });
+
 
         //кнопка запросы
         JQ('#b2').click(function () {
@@ -65,6 +69,7 @@ JQ(document).ready(function () {
             else { alert('Клиент по таким данным в SD не найден') }
         });
 
+
         //кнопка создать запрос
         JQ('#b3').click(function () {
             checkEmp();
@@ -75,15 +80,21 @@ JQ(document).ready(function () {
                 link = 'http://sm-sue.fsfk.local/sd/operator/#add%3AserviceCall%24request%3Aemployee%24' + emplnumber + '%3Arequest!%7B%22fast%22:%22true%22%7D';
             }
             else { link = 'http://sm-sue.fsfk.local/sd/operator/#add%3AserviceCall%24request%3A%3Arequest!%7B%22fast%22:%22true%22%7D' }
+
+
+            console.log(link)
             open_in_new_window(window.event, link, 'form', 1280, 1024);
         });
 
         //кнопка база знаний
         JQ('#b4').click(function () {
-            atremployeeOperator = JQ('#acf_employeeOperator input').val();
+            atremployeeOperator = jQuery('#acf_employeeOperator input').val();
+            console.log(atremployeeOperator);
             let re = /.+\$/g;
             let empOperlnumber = atremployeeOperator.replace(re, '');
+            console.log(empOperlnumber);
             link = 'http://sm-sue.fsfk.local/sd/operator/#uuid:employee$' + empOperlnumber + '!%7B%22tab%22:%22bb3c9c55-1787-2d08-c323-07a01ec644c6%22%7D'
+            console.log(link)
             open_in_new_window(window.event, link, 'form', 1280, 1024);
         });
 
@@ -97,13 +108,13 @@ JQ(document).ready(function () {
 
 
 try {
-    let jsonInfoOperator = JQ('#acf_etf1 p')[0].innerText;
+    let jsonInfoOperator = jQuery('#acf_etf1 p')[0].innerText;
     let jsonParse = JSON.parse(jsonInfoOperator);
 
-    JQ('#acf_firstNameOperator input').val(jsonParse["firstNameOperator"]);
-    JQ('#acf_lastNameOperator input').val(jsonParse["lastNameOperator"]);
-    JQ('#acf_emailOperator input').val(jsonParse["emailOperator"]);
-    JQ('#acf_innOperator input').val('7709895509');
+    jQuery('#acf_firstNameOperator input').val(jsonParse["firstNameOperator"]);
+    jQuery('#acf_lastNameOperator input').val(jsonParse["lastNameOperator"]);
+    jQuery('#acf_emailOperator input').val(jsonParse["emailOperator"]);
+    jQuery('#acf_innOperator input').val('7709895509');
     document.getElementById('b1').click();
 } catch {
     console.log("Что-то пошло не так")
